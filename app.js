3b6e4f60395f0b8f99edcfe3f22ac2f9da42945d
hsiaoball <hsiaoball@gmail.com>
2014-10-09 11:32:17 -0700

    fix point add point and float operation problem in JS

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ app.controller('ctrl_index', ['$scope',  // Controller for index page
                   
               $scope.temp=screen_num;
               $scope.screen='';    
+              $scope.dot=0;    
             }
             else{
               $scope.equal();
@@ -91,7 +92,16 @@ app.controller('ctrl_index', ['$scope',  // Controller for index page
                 default:
                     return;                    
             }
-          $scope.screen=$scope.temp.toString();
+          screen_num=$scope.temp.toString();
+          var a=screen_num.split(".");          
+          if(a.length>1){//for java floating operation
+              a[1]=Number('0.'.concat(a[1]));//for using toFix
+              a[1]=Number(a[1].toFixed(13));//for truncate 0s in the tail
+              $scope.screen=a[0].concat('.').concat(a[1].toString().substring(2, 15));
+          }
+          else{
+              $scope.screen=screen_num;
+          }
           $scope.temp=0;
           $scope.dot=0;
           done=1;
